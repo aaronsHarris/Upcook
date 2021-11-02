45516920d24c3a399e783e6f3bbb0615fe6c0022
Aaron Harris <aaronsharris@gmail.com>
2021-11-02 18:44:14 -0500

    fixed application_record added application_controller, user register working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     if @user.save
       @token = encode({id: @user.id})
       render json: {
-        user: @user.attributes.except('password_digest'),
+        user: @user.attributes.except("password_digest"),
         token: @token
         }, status: :created
     else
@@ -50,6 +50,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:username, :email, :password)
+      params.require(:user).permit( :username, :email, :password )
     end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,13 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
-  SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
-
-  def encode(payload, exp = 24.hours.from_now)
-    payload[:exp] = exp.to_i
-    JWT.encode(payload, SECRET_KEY)
-  end
-
-  def decode(token)
-    decoded = JWT.decode(token, SECRET_KEY)[0]
-    HashWithIndifferentAccess.new decoded
-  end
+  self.abstract_class = true
 end
