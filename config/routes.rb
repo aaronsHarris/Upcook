ec92e06c8427a31d6b38e3fc2467c4af111e0b2c
Aaron Harris <aaronsharris@gmail.com>
2021-11-02 21:03:57 -0500

    scaffold for recipes, ingredients, & directions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :directions
+  resources :ingredients
+  resources :users
+  resources :recipes
   post '/auth/login', to: 'authentications#login'
   get '/auth/verify', to: 'authentications#verify'
 
-  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
