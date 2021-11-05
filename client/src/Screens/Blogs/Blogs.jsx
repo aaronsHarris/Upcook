cc55231afa5179e8a554b5e8fc21b0fcce9ef7bf
Aaron Harris <aaronsharris@gmail.com>
2021-11-05 08:18:54 -0500

    map out blogs screen

--- client/src/Screens/Blogs/Blogs.jsx
+++ client/src/Screens/Blogs/Blogs.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
-const Blogs = () => {
-    return (
-        <div>
-            <h1>blogs</h1>
-        </div>
-    )
-}
+const Blogs = (props) => {
+  const { blogs } = props;
+  return (
+    <div>
+      <h3>Blogs</h3>
+      <Link to="/blogss/new">add</Link>
+      <div>
+        {blogs.map((blog) => (
+          <Link to={`/blogs/${blog.id}`}>
+            <div className="blogCard" key={blog.id}>
+              <p>{blog.title}</p>
+              <p>{blog.tagline}</p>
+              <img
+                className="blogImage"
+                src={blog.image_url}
+                alt="recipeItem"
+              />
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Blogs
+export default Blogs;
