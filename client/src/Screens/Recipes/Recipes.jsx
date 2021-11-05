37e5d4c7070a908b95f36332734bec89217edfcc
Aaron Harris <aaronsharris@gmail.com>
2021-11-05 17:46:29 -0500

    added initial styling of recipes.jsx

--- client/src/Screens/Recipes/Recipes.jsx
+++ client/src/Screens/Recipes/Recipes.jsx
@@ -5,27 +5,37 @@ const Recipes = (props) => {
   const { recipes } = props;
 
   return (
-    <div  className="recipeContainer">
+    
+    <div className="recipe-wrapper">
+      <div className="recipe-header">
+        <div>
       <h3>Recipes</h3>
-      <Link to="/recipes/new">add</Link>
-      <div>
-        <div className="grid-space">
+
+        </div>
+      <Link to="/recipes/new">
+      <button className="button-77">add</button>
+        </Link>
+        </div>
+    <div  className="recipeContainer">
         {recipes.map((recipe) => (
           <Link to={`/recipes/${recipe.id}`}>
             <div className="card" key={recipe.id}>
               {/* <h1>{recipe.name}</h1> */}
+              <div className="image-container">
               <img
                 className="recipeImage"
                 src={recipe.image_url}
                 alt="recipeItem"
               />
+              </div>
             </div>
           </Link>
         ))}
-          </div>
-      </div>
+         
+      
     </div>
-  );
+      </div>
+      );
 };
 
 export default Recipes;
