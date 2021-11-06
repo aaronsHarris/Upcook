c70901cf662bc248aadf591c3861a4fa1593a536
Aaron Harris <aaronsharris@gmail.com>
2021-11-06 15:53:09 -0500

    basic style for recipe details page

--- client/src/Screens/RecipeDetail/RecipeDetail.jsx
+++ client/src/Screens/RecipeDetail/RecipeDetail.jsx
@@ -17,35 +17,32 @@ const RecipeDetail = (props) => {
   }, [id]);
 
   return (
-  
-      <div className="recipe-detail-wrapper">
-        <div className="recipe-detail-header">
-          {/* <div className="recipe-detail-title">
-            <h3>recipe detail</h3>
-          </div> */}
-          <div  classNmae="back-button">
-          <Link to="/recipes">
-            <button className="button-77">
-              <FaIcons.FaArrowLeft />
-            </button>
-          </Link>
-          </div>
-        </div>
-        <div className="recipe-detail-container">
-          <h2>{recipe?.name}</h2>
-          <img src={recipe?.image_url} alt="recipeDetail" />
-          <h2>{recipe?.time}</h2>
-          <h2>{recipe?.difficulty}</h2>
-          <h3>{recipe?.description}</h3>
-          <p>{recipe?.ingredients}</p>
-          <p>{recipe?.directions}</p>
-        </div>
-        <button onClick={() => handleRecipeDelete(recipe.id)}>delete</button>
-        <Link to={`/recipes/${id}/edit`}>
-          <button>edit</button>
+    <div className="flex flex-col min-h-screen justify-center">
+      <div className="grid grid-cols-3 my-10 text-center">
+        <Link to="/recipes">
+          <button class="flex items-center p-4  transition ease-in duration-200 uppercase rounded-full hover:bg-green-400 hover:text-white border-2 border-gray-900 focus:outline-none">
+            <FaIcons.FaArrowLeft />
+          </button>
         </Link>
+        <div className="text-5xl">{recipe?.name}</div>
       </div>
-    
+      <div className="flex flex-col justify-center items-center mb-10">
+        <img className="w-96 " src={recipe?.image_url} alt="recipeDetail" />
+        <div className="text-xl">
+          <div> Description: {recipe?.description}</div>
+          <div>Time: {recipe?.time}</div>
+          <div>Difficulty: {recipe?.difficulty}</div>
+          <div>Ingredients: {recipe?.ingredients}</div>
+          <div>Directions: {recipe?.directions}</div>
+        </div>
+      </div>
+      <div className="flex justify-end">
+      <button className="mx-10" onClick={() => handleRecipeDelete(recipe.id)}>delete</button>
+      <Link to={`/recipes/${id}/edit`}>
+        <button className="mx-10">edit</button>
+      </Link>
+      </div>
+    </div>
   );
 };
 
