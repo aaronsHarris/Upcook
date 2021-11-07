7e8bc1815f5cdbf966a105cdec8a633c6c04cb79
Aaron Harris <aaronsharris@gmail.com>
2021-11-07 13:55:29 -0600

    added styling to home screen

--- client/src/Screens/RecipeEdit/RecipeEdit.jsx
+++ client/src/Screens/RecipeEdit/RecipeEdit.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom"; 
-
+import * as AiIcons from "react-icons/ai";
+// import './blogEdit.css'
+import { Link } from "react-router-dom";
 const RecipeEdit = (props) => {
   const [formData, setFormData] = useState({
     name: "",
@@ -51,70 +53,215 @@ const RecipeEdit = (props) => {
   };
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        handleRecipeUpdate(id, formData);
-      }}
-    >
-      <h1>Edit Food</h1>
-      <div>
-        Name:
-        <input type="text" name="name" value={name} onChange={handleChange} />
-      </div>
-      <div>
-        time:
-        <input type="text" name="time" value={time} onChange={handleChange} />
-      </div>
-      <div>
-        difficulty:
-        <input
-          type="text"
-          name="difficulty"
-          value={difficulty}
-          onChange={handleChange}
+    <div className="absolute w-screen h-screen flex">
+      <div className="hidden lg:block w-6/12 h-full">
+        <img
+          src="https://res.cloudinary.com/dbdrox2p9/image/upload/v1636308988/upcook/istockphoto-480379752-612x612_j7felv.jpg"
+          className="w-full h-full object-cover  shadow-lg "
         />
       </div>
-      <div>
-        description:
-        <input
-          type="text"
-          name="descricption"
-          value={description}
-          onChange={handleChange}
-        />
+      <Link to='/recipes'>
+        <div className="close-button"><AiIcons.AiOutlineClose /></div>
+        </Link>
+      <div className="w-full lg:w-7/12 overflow-scroll py-24 relative">
+        <form
+          className="w-5/6 sm:w-1/2 mx-auto text-center"
+          onSubmit={(e) => {
+               e.preventDefault();
+                handleRecipeUpdate(id, formData);
+             }}
+        >
+          <img
+            src="https://res.cloudinary.com/dbdrox2p9/image/upload/v1636248110/upcook/0d7b260309e34d43b8fce143e4c99b6b_1_kaghfg.png"
+            className="h-24 block mx-auto"
+          />
+          <div className="mt-10">
+            <h2 className="text-3xl font-bold text-gray-800">Update your Recipe</h2>
+            
+          </div>
+          <div className="mt-12">
+            <div class="w-full mt-4">
+              <input
+                class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md dark:bg-gray-800  dark:placeholder-gray-400 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                type="text"
+                placeholder="Name"
+                name="name"
+                value={name}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div class="w-full mt-4">
+              <input
+                class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md dark:bg-gray-800 focus:border-blue-500
+                focus:outline-none focus:ring"
+                type="text"
+                placeholder="Time"
+                name="time"
+                value={time}
+                onChange={handleChange}
+              />
+            </div>
+            <div class="w-full mt-4">
+              <input
+                class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md dark:bg-gray-800 focus:border-blue-500
+                focus:outline-none focus:ring"
+                type="text"
+                placeholder="Difficulty"
+                name="difficulty"
+                value={difficulty}
+                onChange={handleChange}
+              />
+            </div>
+            <div class="w-full mt-4">
+              {/* <input
+                class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md dark:bg-gray-800 focus:border-blue-500
+                focus:outline-none focus:ring"
+                type="text"
+                placeholder="Description"
+                name="description"
+                value={description}
+                onChange={handleChange}
+              /> */}
+              <textarea
+                class="form-textarea block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md dark:bg-gray-800 focus:border-blue-500
+                focus:outline-none focus:ring"
+                rows="4"
+                placeholder="Description"
+                name="description"
+                value={description}
+                onChange={handleChange}
+              ></textarea>
+            </div>
+            <div class="w-full mt-4">
+              <input
+                class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md dark:bg-gray-800 focus:border-blue-500
+                focus:outline-none focus:ring"
+                type="text"
+                placeholder="Image"
+                name="image_url"
+                value={image_url}
+                onChange={handleChange}
+              />
+            </div>
+            <div class="w-full mt-4">
+              {/* <input
+                class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md dark:bg-gray-800 focus:border-blue-500
+                focus:outline-none focus:ring"
+                type="text"
+                placeholder="Ingredients"
+                name="ingredients"
+                value={ingredients}
+                onChange={handleChange}
+              /> */}
+              <textarea
+                class="form-textarea block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md dark:bg-gray-800 focus:border-blue-500
+                focus:outline-none focus:ring"
+                rows="3"
+                type="text"
+                placeholder="Ingredients"
+                name="ingredients"
+                value={ingredients}
+                onChange={handleChange}
+              ></textarea>
+            </div>
+            <div class="w-full mt-4">
+              {/* <input
+                class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md dark:bg-gray-800 focus:border-blue-500
+                focus:outline-none focus:ring"
+                type="text"
+                placeholder="Directions"
+                name="directions"
+                value={directions}
+                onChange={handleChange}
+              /> */}
+              <textarea
+                class="form-textarea block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md dark:bg-gray-800 focus:border-blue-500
+                focus:outline-none focus:ring"
+                rows="3"
+                type="text"
+                placeholder="Directions"
+                name="directions"
+                value={directions}
+                onChange={handleChange}
+              ></textarea>
+            </div>
+
+            <div class="flex items-center justify-between mt-4">
+              <div class="text-sm text-gray-600 dark:text-gray-200 hover:text-gray-500"></div>
+              <button class="px-4 py-2 leading-5 text-white transition-colors duration-200 transform bg-gray-700 rounded hover:bg-gray-600 focus:outline-none">
+                Update
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
-      <div>
-        image:
-        <input
-          type="text"
-          name="image_url"
-          value={image_url}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        ingredients:
-        <input
-          type="text"
-          name="ingredient"
-          value={ingredients}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        directions:
-        <input
-          type="text"
-          name="directions"
-          value={directions}
-          onChange={handleChange}
-        />
-      </div>
-      <br />
-      <button>Submit</button>
-    </form>
+    </div>
   );
 };
 
 export default RecipeEdit;
+
+    // <form
+    //   onSubmit={(e) => {
+    //     e.preventDefault();
+    //     handleRecipeUpdate(id, formData);
+    //   }}
+    // >
+    //   <h1>Edit Food</h1>
+    //   <div>
+    //     Name:
+    //     <input type="text" name="name" value={name} onChange={handleChange} />
+    //   </div>
+    //   <div>
+    //     time:
+    //     <input type="text" name="time" value={time} onChange={handleChange} />
+    //   </div>
+    //   <div>
+    //     difficulty:
+    //     <input
+    //       type="text"
+    //       name="difficulty"
+    //       value={difficulty}
+    //       onChange={handleChange}
+    //     />
+    //   </div>
+    //   <div>
+    //     description:
+    //     <input
+    //       type="text"
+    //       name="descricption"
+    //       value={description}
+    //       onChange={handleChange}
+    //     />
+    //   </div>
+    //   <div>
+    //     image:
+    //     <input
+    //       type="text"
+    //       name="image_url"
+    //       value={image_url}
+    //       onChange={handleChange}
+    //     />
+    //   </div>
+    //   <div>
+    //     ingredients:
+    //     <input
+    //       type="text"
+    //       name="ingredient"
+    //       value={ingredients}
+    //       onChange={handleChange}
+    //     />
+    //   </div>
+    //   <div>
+    //     directions:
+    //     <input
+    //       type="text"
+    //       name="directions"
+    //       value={directions}
+    //       onChange={handleChange}
+    //     />
+    //   </div>
+    //   <br />
+    //   <button>Submit</button>
+    // </form>
