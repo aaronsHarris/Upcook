d161cad5bd9481898a160e45ea9542066ff07ea1
Aaron Harris <aaronsharris@gmail.com>
2021-11-07 12:03:39 -0600

    added style and back button to blogEdit

--- client/src/Screens/BlogCreate/BlogCreate.jsx
+++ client/src/Screens/BlogCreate/BlogCreate.jsx
@@ -66,7 +66,7 @@ const BlogCreate = (props) => {
                 onChange={handleChange}
               />
             </div>
-            
+
             <div class="w-full mt-4">
               <textarea
                 class="form-textarea block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md dark:bg-gray-800 focus:border-blue-500
